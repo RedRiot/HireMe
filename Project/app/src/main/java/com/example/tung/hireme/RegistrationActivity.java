3fdaf2ccc92f16b099ecc504888b399f606e8071
BalazsSalfay <balazs.salfay@gmail.com>
2018-04-13 17:14:29 +0200

    feat(hireme) firebase database filled with data

--- Project/app/src/main/java/com/example/tung/hireme/RegistrationActivity.java
+++ Project/app/src/main/java/com/example/tung/hireme/RegistrationActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,10 +16,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
     private Button register;
-    private EditText mEmail, mPassword;
+    private EditText mEmail, mPassword, mName;
+
+    private RadioGroup radioGroup;
+
     private FirebaseAuth auth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
 
@@ -44,17 +51,38 @@ public class RegistrationActivity extends AppCompatActivity {
         register = (Button) findViewById(R.id.register);
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
+        mName = (EditText) findViewById(R.id.name);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int selectId = radioGroup.getCheckedRadioButtonId();
+
+                final RadioButton radioButton = (RadioButton) findViewById(selectId);
+
+                if (radioButton.getText() == null) {
+                    return;
+                }
+
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
-                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                final String name = mName.getText().toString();
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationActivity.this,
+                        new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (!task.isSuccessful()) {
                             Toast.makeText(RegistrationActivity.this, "Sign up Error", Toast.LENGTH_SHORT).show();
+                        } else {
+                            String userId = auth.getCurrentUser().getUid();
+                            DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference()
+                                    .child("Users")
+                                    .child(radioButton.getText().toString())
+                                    .child(userId)
+                                    .child("name");
+                            currentUserDb.setValue(name);
                         }
                     }
                 });
