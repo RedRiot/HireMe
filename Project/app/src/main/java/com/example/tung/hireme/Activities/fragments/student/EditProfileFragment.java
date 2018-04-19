13ef1dcce3d001049952627afecc736c692cb85a
Nguyen Quoc Tung <quoc.tung@outlook.com>
2018-04-19 13:57:07 +0200

    feat(EditProfileFragment)create EditProfile fragment and remove unnecessary back button

--- Project/app/src/main/java/com/example/tung/hireme/Activities/fragments/student/EditProfileFragment.java
+++ Project/app/src/main/java/com/example/tung/hireme/Activities/fragments/student/EditProfileFragment.java
@@ -1,6 +1,172 @@
 package com.example.tung.hireme.Activities.fragments.student;
 
-import android.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.tung.hireme.Activities.activities.StudentActivity;
+import com.example.tung.hireme.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditProfileFragment extends Fragment {
+    private EditText mNameField, mSummmaryField;
+    private Button mConfirm;
+    private ImageView mProfileImage;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference mCostumerDatabase;
+
+    private String userId, name, summary, profileImageUrl;
+
+    private Uri resultUri;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_setting, container, false);
+        final String STUDENT = "Student";
+        mNameField = (EditText) view.findViewById(R.id.names);
+        mSummmaryField = (EditText) view.findViewById(R.id.summaries);
+        mProfileImage = (ImageView) view.findViewById(R.id.profileImage);
+
+
+        mConfirm = (Button) view.findViewById(R.id.confirm);
+        firebaseAuth = FirebaseAuth.getInstance();
+        userId = firebaseAuth.getCurrentUser().getUid();
+        mCostumerDatabase = FirebaseDatabase.getInstance()
+                .getReference()
+                .child("Users")
+                .child(STUDENT)
+                .child(userId);
+        getUserInfo();
+
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+            }
+        });
+        mConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveUserInformation();
+                Intent intent = new Intent(getActivity(), StudentActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        return view;
+
+    }
+
+    private void getUserInfo() {
+        mCostumerDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if (map.get("name") != null) {
+                        name = map.get("name").toString();
+                        mNameField.setText(name);
+                    }
+                    if (map.get("summary") != null) {
+                        summary = map.get("summary").toString();
+                        mSummmaryField.setText(summary);
+                    }
+                    if (map.get("profileImageUrl") != null) {
+                        profileImageUrl = map.get("profileImageUrl").toString();
+                        Glide.with(getActivity().getApplication()).load(profileImageUrl).into(mProfileImage);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void saveUserInformation() {
+        name = mNameField.getText().toString();
+        summary = mSummmaryField.getText().toString();
+
+        Map userInfo = new HashMap();
+        userInfo.put("name", name);
+        userInfo.put("summary", summary);
+        mCostumerDatabase.updateChildren(userInfo);
+        if (resultUri != null) {
+            StorageReference filePath = FirebaseStorage.getInstance().getReference()
+                    .child("profileImages")
+                    .child(userId);
+            Bitmap bitmap =null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getApplication().getContentResolver(), resultUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, byteArray);
+            byte[] data = byteArray.toByteArray();
+            UploadTask uploadTask = filePath.putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    getActivity().finish();
+                }
+            });
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downLoadUrl = taskSnapshot.getDownloadUrl();
+                    Map userInfo = new HashMap();
+                    userInfo.put("profileImageUrl", downLoadUrl.toString());
+                    mCostumerDatabase.updateChildren(userInfo);
+                    getActivity().finish();
+                }
+            });
+
+        } else {
+            getActivity().finish();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK){
+            final Uri imageUri = data.getData();
+            resultUri = imageUri;
+            mProfileImage.setImageURI(resultUri);
+        }
+    }
 }
+
+
