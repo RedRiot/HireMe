a562a847d185bdb33d784ad193d69b61f1432e92
Nguyen Quoc Tung <quoc.tung@outlook.com>
2018-05-04 17:01:25 +0200

    feat(savedStudentFragment) added working recyclerview

--- Project/app/src/main/java/com/example/tung/hireme/Activities/adapters/SavedStudentAdapter.java
+++ Project/app/src/main/java/com/example/tung/hireme/Activities/adapters/SavedStudentAdapter.java
@@ -1,9 +1,7 @@
 package com.example.tung.hireme.Activities.adapters;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
-import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +9,7 @@ import android.widget.TextView;
 
 import com.example.tung.hireme.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,22 +21,27 @@ public class SavedStudentAdapter extends RecyclerView.Adapter<SavedStudentAdapte
     private Context context;
     private int lastSelectedPosition;
 
-    public SavedStudentAdapter(Context context, List<String> names) {
-        this.names = names;
+    public SavedStudentAdapter(Context context) {
+        this.names = new ArrayList<>();
         this.context = context;
+    }
+
+    public void addNames(List<String> names) {
+        this.names.addAll(names);
+        notifyDataSetChanged();
     }
 
     @Override
     public SavedStudentAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.saved_profile_fragment, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.student_item, parent, false);
         SavedStudentAdapter.ViewHolder viewHolder = new SavedStudentAdapter.ViewHolder(view);
         return viewHolder;
-
     }
 
     @Override
     public void onBindViewHolder(SavedStudentAdapter.ViewHolder holder, int position) {
         String name = names.get(position);
+        holder.name.setText(name);
 
     }
 
--- Project/app/src/main/java/com/example/tung/hireme/Activities/fragments/company/SavedProfilesFragment.java
+++ Project/app/src/main/java/com/example/tung/hireme/Activities/fragments/company/SavedProfilesFragment.java
@@ -3,21 +3,39 @@ package com.example.tung.hireme.Activities.fragments.company;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.tung.hireme.Activities.adapters.SavedStudentAdapter;
 import com.example.tung.hireme.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SavedProfilesFragment extends Fragment {
     private RecyclerView recyclerView;
+    private SavedStudentAdapter adapter;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //this should changed with RecycleView
         View view = inflater.inflate(R.layout.saved_profile_fragment, container, false);
+        adapter = new SavedStudentAdapter(getActivity());
+        recyclerView = view.findViewById(R.id.apilistcalendars);
+        LinearLayoutManager recyclerLayoutManager = new LinearLayoutManager(this.getContext());
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+        DividerItemDecoration dividerItemDecoration =
+                new DividerItemDecoration(recyclerView.getContext(), recyclerLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        List<String> names = new ArrayList<>(Arrays.asList("adasd", "asdasdasd", "fsdfr"));
+        adapter.addNames(names);
+        recyclerView.setAdapter(adapter);
 
         return view;
     }
